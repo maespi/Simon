e66ab0d9bb35236282069e1c9dba0c76002de939
maespi <marcausejo@hotmail.com>
2018-02-06 07:16:58 +0100

    -Toolbar Funciona

--- app/src/main/java/com/example/marca/simonsays/MainSimon.java
+++ app/src/main/java/com/example/marca/simonsays/MainSimon.java
@@ -2,9 +2,9 @@ package com.example.marca.simonsays;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
-import android.view.MenuItem;
 
 public class MainSimon extends AppCompatActivity {
 
@@ -12,6 +12,9 @@ public class MainSimon extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_simon);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(myToolbar);
+
     }
 
     @Override
